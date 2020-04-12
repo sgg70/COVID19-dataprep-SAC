2221314a963ecb528663b314e2b50b4e6f7d31b0
Stefan <web@esfera.de>
2020-04-12 14:24:05 -0600

    Merge branch 'master' of https://github.com/sgg70/COVID19-dataprep-SAC
    # Conflicts:
    #	COVIDdataprep.py
    #	requirements.txt

--- COVIDdataprep.py
+++ COVIDdataprep.py
@@ -32,7 +32,7 @@ import urllib.request
 import datetime
 ### technically, the other imports should also all be here and not in the functions defs, being lazy for now
 
-#
+# test
 
 def load_continent_latlon():
     load_saved = 'continentslatlon.csv'
